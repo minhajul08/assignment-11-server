33fa8a2817fc97f11ea2028d2b6950e00f0ec602
minhajul08 <minhazmehadi61@gmail.com>
2024-06-19 00:46:19 +0600

    added env file to save private info

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const corsOptions ={
 }
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.01tlpf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
