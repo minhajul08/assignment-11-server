e763c573464788d1332989ecd8e7a2753bd738c8
minhajul08 <minhazmehadi61@gmail.com>
2024-06-21 19:30:03 +0600

    rooms data added from db

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const corsOptions ={
     optionSuccessStatus: 200,
 }
 
-
+// pk
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.01tlpf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
