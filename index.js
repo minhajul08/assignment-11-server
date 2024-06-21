3bb054796b7cdfa7de974f4ec612828c8dd5cb74
minhajul08 <minhazmehadi61@gmail.com>
2024-06-21 20:03:09 +0600

    crated initial server with mongodb

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require ('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require ('dotenv').config()
-const port = process.env.PORT || 5000
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
 
-const app = express ()
+
+// middleware
+app.use (cors ());
+app.use (express.json ());
 
 
-const corsOptions ={
-    origin: ['http://localhost:5173'],
-    credentials: true,
-    optionSuccessStatus: 200,
-}
 
-// pk
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.01tlpf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,33 +27,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const bookingRoom = client.db ('roomBooking').collection ('rooms')
-
-    // all jobs data
-    app.get ('/rooms', async (req,res) => {
-      const result = await bookingRoom.find ().toArray ()
-      res.send (result)
-    })
-   
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.use (cors(corsOptions))
-app.use (express.json())
-
 app.get ('/', (req,res) => {
-    res.send ('Hello world')
-   
-
+    res.send ('Hotel server is running')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen (port, () => {
+    console.log (`Hotel Server is running on port ${port}`)
+})
